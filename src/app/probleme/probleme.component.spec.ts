61e39ffb729f3888f2a4bc2a7f6c443916aab89e
Gabriel Levesque-Duval <glevesqueduval@gmail.com>
2019-02-28 14:12:34 -0500

    Ajout test unitaire prénom invalide avec 2 caractères

---
+++ src/app/probleme/probleme.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProblemeComponent } from './probleme.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+describe('ProblemeComponent', () => {
+  let component: ProblemeComponent;
+  let fixture: ComponentFixture<ProblemeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule,
+        AngularFontAwesomeModule],
+
+      declarations: [ ProblemeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProblemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+   it('should create', () => {
+     expect(component).toBeTruthy();
+   });
+
+  it('champ prénom doit être invalide avec 2 caractères', () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(2));
+    expect(zone.valid).toBeFalsy();
+
+  });
+  
+});
